a80d577f408f9ccbbf3dd98145f5c572ae821545
Goory al Hamed <gooryalhamed@hotmail.com>
2017-03-09 22:06:27 +0300

    almost done

--- js/code.js
+++ js/code.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
 
-	$('a').click(function(e){
+	$('#sizing-addon2').click(function(e){
 		$('#imgLoading').fadeIn();
 		$('#error').fadeOut();
 		e.preventDefault();
 		var terms=$('#searchTerms').val();
+		/*
 		$.get(`https://api.github.com/search/repositories?q=${terms}`,function(data){
 			
 			$.each(data['items'],function(i,itm)
@@ -18,7 +19,18 @@ $(document).ready(function(){
 		}).fail(function(error){
 			$('#imgLoading').fadeOut();
 			$('#error').fadeIn();
-		})
+		});
+		*/
 
-	})
+		$.get(`https://api.github.com/users/${searchTerms}/repos`, function(data){
+			for(i=0; i < data.length; i+2){
+				$('#result').append(`<a href="${data[i+1]['html_url']}">${data[i]['name']}</a>`);
+			}
+		$('#imgLoading').fadeOut();
+		}).fail(function(){
+			$('#imgLoading').fadeOut();
+			$('#error').fadeIn();
+		});
+		
+	});
 })
