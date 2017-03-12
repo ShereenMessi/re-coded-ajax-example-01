b5f851c6bc0f420b88b604b9aa46144ecebd79c8
Goory al Hamed <gooryalhamed@hotmail.com>
2017-03-12 11:15:19 +0300

    two type of search

--- js/code.js
+++ js/code.js
@@ -1,16 +1,19 @@
 $(document).ready(function(){
+	searchRepo();
+	searchUser();
+})
 
+function searchRepo(){
 	$('#sizing-addon2').click(function(e){
 		$('#imgLoading').fadeIn();
 		$('#error').fadeOut();
 		e.preventDefault();
 		var terms=$('#searchTerms').val();
-		/*
+		
 		$.get(`https://api.github.com/search/repositories?q=${terms}`,function(data){
 			
 			$.each(data['items'],function(i,itm)
-			{
-				
+			{	
 				$('#result').append(`<a href="${itm['html_url']}">${itm['name']}</a><br>`)
 				$('#result').append(`<p>${itm['description']}</p>`)
 				$('#result').append(`Owner <a href='${itm['owner']['html_url']}'>${itm['owner']['login']}</a>`)
@@ -20,8 +23,15 @@ $(document).ready(function(){
 			$('#imgLoading').fadeOut();
 			$('#error').fadeIn();
 		});
-	
-	*/
+	});
+}
+
+function searchUser(){
+	$('#sizing-addon2').click(function(e){
+		$('#imgLoading').fadeIn();
+		$('#error').fadeOut();
+		e.preventDefault();
+		var terms=$('#searchTerms').val();
 		$.get(`https://api.github.com/users/${terms}/repos`, function(data){
 			for(i=0; i < data.length; i++){
 				$('#result').append(`<a href="${data[i]['html_url']}" target="_blank">${data[i]['name']}</a>`);
@@ -32,6 +42,5 @@ $(document).ready(function(){
 			$('#imgLoading').fadeOut();
 			$('#error').fadeIn();
 		});
-	
 	});
-})
+}
